6eab224bc325c250bb50e13132db6167cc17a8ea
marukun712 <marukun530@gmail.com>
2026-06-10 00:07:23 +0900

    path修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	base: "/kokoro/",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(import.meta.dirname, "index.html"),
+				audio: resolve(import.meta.dirname, "audio.html"),
+				template: resolve(import.meta.dirname, "template.html"),
+			},
+		},
+	},
 });
