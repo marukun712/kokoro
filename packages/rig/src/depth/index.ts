20c5ebc262bab96622810faf0525b7cab6825387
marukun712 <marukun530@gmail.com>
2026-06-25 16:20:46 +0900

    依存関係の修正

--- packages/rig/src/depth/index.ts
+++ packages/rig/src/depth/index.ts
@@ -51,7 +51,9 @@ export async function getDepth(
 	const dataURL = tempCanvas.toDataURL("image/png");
 
 	return new Promise((resolve, reject) => {
-		const worker = new Worker(new URL("./worker.ts", import.meta.url), {
+		const url = new URL("./worker.ts", import.meta.url);
+
+		const worker = new Worker(url, {
 			type: "module",
 		});
 
